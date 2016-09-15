75f4fef3a42f1dc67e310b4905819a552c572e33
Andrew Accuardi and Omid Ashtari <andrew@>
2016-09-14 19:39:54 -0700

    add jquery code for subtract, multiply and divide

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,15 @@ var add = function (number1, number2){
 	return number1 + number2;
 };
 
-
-
-
-
 var subtract = function (number1, number2){
 	return number1 - number2;
 };
-
-
-
-/*var result = subtract(number1, number2);
-alert(result);
-
-var multiply = function (number1, number2){
-	return number1 * number2;
-};
-
-
-
-var result = multiply(number1, number2);
-alert(result);
 
 var divide = function (number1, number2){
 	return number1 / number2;
 };
 
 
-
-var result = divide(number1, number2);
-alert(result);
-*/
 //Front-end logic
 $(document).ready(function() {
 	$("form#add").submit(function(event){
@@ -46,12 +24,32 @@ $(document).ready(function() {
 });
 
 
-/*var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
+$(document).ready(function() {
+	$("form#subtract").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#subtract1").val());
+		var number2 = parseInt($("#subtract2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+});
 
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
+$(document).ready(function() {
+	$("form#multiply").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#multiply1").val());
+		var number2 = parseInt($("#multiply2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+});
 
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
-*/
+$(document).ready(function() {
+	$("form#divide").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#divide1").val());
+		var number2 = parseInt($("#divide2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+});
