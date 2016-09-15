4faad567f0d4cb45b28440fb0c6663505cf9c575
Andrew Accuardi and Omid Ashtari <andrew@>
2016-09-14 19:24:22 -0700

    add all cloned files from calculator, add jquery functions to take information from input fields in the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,26 @@
+//Back-end logic
 var add = function (number1, number2){
 	return number1 + number2;
 };
 
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
 
-var result = add(number1, number2);
-alert(result);
+
+
 
 var subtract = function (number1, number2){
 	return number1 - number2;
 };
 
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
 
-var result = subtract(number1, number2);
+
+/*var result = subtract(number1, number2);
 alert(result);
 
 var multiply = function (number1, number2){
 	return number1 * number2;
 };
 
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
+
 
 var result = multiply(number1, number2);
 alert(result);
@@ -32,8 +29,29 @@ var divide = function (number1, number2){
 	return number1 / number2;
 };
 
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
+
 
 var result = divide(number1, number2);
 alert(result);
+*/
+//Front-end logic
+$(document).ready(function() {
+	$("form#add").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+});
+
+
+/*var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter another number:"));
+
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter another number:"));
+
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter another number:"));
+*/
